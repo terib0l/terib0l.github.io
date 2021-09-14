4369dc1266ca64acc4e45751cbf68db2640b7807
terib0l <taiyakiou@gmail.com>
2021-09-14 23:39:03 +0900

    add typewriter func in js

--- js/style.js
+++ js/style.js
@@ -1,3 +1,41 @@
+// Text Animation like typing in title
+const title = document.getElementById("title");
+const titleText = title.textContent;
+
+function typewriter(arg) {
+  let element = document.querySelector(arg.element);
+  let text = arg.text.split("");
+  let speed = arg.speed;
+
+  console.log(element, text, speed);
+
+  window.setTimeout(function () {
+    element.textContent = "";
+    text.forEach((c, index) => {
+      setTimeout(() => {
+        element.textContent += c;
+      }, speed * index);
+    });
+  }, 1000);
+};
+
+typewriter({
+  element: "#title",
+  text: "TERI0L.GITHUB.IO",
+  speed: 60
+});
+
+// Change color of icons on mouseover
+const icon = document.getElementById("icon");
+
+icon.addEventListener("mouseover", function(event){
+  event.target.style.color = "#666666";
+});
+icon.addEventListener("mouseout", function(event){
+  event.target.style.color = "#fd6a02";
+});
+
+// Shake icons
 function fluffy() {
   $('.icons').animate({marginTop: '-=12px'}, 800).animate({marginTop: '+=12px'}, 800);
   setTimeout('fluffy()', 1600);
